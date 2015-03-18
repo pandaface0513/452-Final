7bbca19480ed2a79b9f7babc12f3c622eba6f05f
Kevin Choy <kgchoy@sfu.ca>
2015-03-17 20:01:28 -0700

    detect clicks inside a rect element based on collision dectection... sort of working

--- draw.js
+++ draw.js
@@ -1,5 +1,9 @@
 var objArr = [];
 
+// dimensions of the node elements
+var WIDTH = 20;
+var HEIGHT = 20;
+
 var svgContainer;
 
 window.onload = function(){
@@ -12,14 +16,71 @@ window.onload = function(){
     //mouse click listener
     svgContainer.on("click", function(){
         var mouse = d3.mouse(this);
+
+
+        
+
         var o = {
             ID: objArr.length,
             X: mouse[0],
             Y: mouse[1]
         };
-        log(o);
-        objArr.push(o);
+        // log(o);
+        var len = objArr.length;
+        for(var i = 0; i < len; i++){
+            if((mouse[0] > objArr[i].X - WIDTH) && (mouse[0] < (objArr[i].X + WIDTH)) &&
+                (mouse[1] > objArr[i].Y - HEIGHT) && (mouse[1] < (objArr[i].Y + HEIGHT))){
+                
+
+            }
+        }
+        var breakLoop = false;
+        var i = 0;
+        
+        console.log("outer i = " + i + ", len = " + len);
+        while (!breakLoop && i < len - 1) {
+            console.log("inner i = " + i + ", len = " + len);
+
+            /*
+             *  
+             *  Determine if mouse click coordinates fall within the bounds of an existing node.
+             *  If it does, deal with the node with the corresponding ID.
+             *  We also do an additional check to ensure that that it doesn't overlap to the node's left
+             *  left or top edge, which we didn't check for earlier.
+             *
+             */
+            if ((mouse[0] > objArr[i].X - WIDTH) && (mouse[0] < (objArr[i].X + WIDTH)) &&
+                (mouse[1] > objArr[i].Y - HEIGHT) && (mouse[1] < (objArr[i].Y + HEIGHT))) {
+                if (mouse[0] > objArr[i].X && mouse[1] > objArr[i].Y) {
+
+
+
+
+                    console.log("clicked inside " + objArr[i].X);
+                    breakLoop = true;
+                    console.log("entering loop");
+                }
+
+            }
+            // for (var i = 0, len = objArray.length; i < len; i++) {
+            //     if (mouse[0] == i[0].X)
+            // }
+            i++;
+        }
+
+        /*
+         *  Only add a new node if it doesn't overlap with another node. If breakLoop is true,
+         *  we already know not to draw the node. 
+         */
+        if (breakLoop == false) {
+            objArr.push(o);
+        }
+
         draw(objArr);
+
+        // var squares = svgContainer.selectAll("rect");
+        // console.log(objArr);
+        
     });
 }
 
@@ -28,10 +89,16 @@ var draw = function(data){
                 .data(data)
                 .enter()
                 .append("svg:rect")
+                .attr("id", function(d){ return "note-id-" + d.ID;})
                 .attr("x", function(d){ return d.X;})
                 .attr("y", function(d){ return d.Y;})
-                .attr("width", 20)
-                .attr("height", 20)
+                .attr("width", WIDTH)
+                .attr("height", HEIGHT)
+                .attr("onclick", function(d) {
+                    console.log("onclick:");
+                    console.log(d);
+                    var idString = "node-id-" + d.ID;
+                })
                 .style("stroke", "black");
 }
 
